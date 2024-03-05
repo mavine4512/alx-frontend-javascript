a98d5a71fedfb3af0c255ca41edbf72838dd831d
mavine4512 <mavineoyugi@gmail.com>
2024-03-05 20:44:40 +0300

    fixed items

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: JSON.stringify(success) });
-    reject(Error("API not working"));
+    if (success) resolve({ status: 200, body: success });
+    reject(Error("The fake API is not working currently"));
   });
 }
 

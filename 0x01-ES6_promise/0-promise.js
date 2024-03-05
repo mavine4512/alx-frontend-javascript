900054a98ddc76633958c51a1c18693a7353ac06
mavine4512 <mavineoyugi@gmail.com>
2024-03-05 13:58:45 +0300

    fixed items

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: JSON.stringify(success) });
+    reject(Error("API not working"));
+  });
+}
